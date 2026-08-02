41ea347c707961ce175710a827d42d3805eeeb92
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-03 01:17:17 +0500

    controls: progressbar: implement ProgressBar

--- controls/progressbar.cpp
+++ controls/progressbar.cpp
@@ -5,19 +5,51 @@
 
 using namespace vgui;
 
-ProgressBar::ProgressBar( int segmentCount ) : Panel()
+ProgressBar::ProgressBar( int segmentCount ) : Panel( 0, 0, 10, 110 ),
+	_segmentCount( segmentCount ), _progress( 0.0f )
 {
 }
 
 void ProgressBar::paintBackground()
 {
+	int wide, tall;
+
+	getPaintSize( wide, tall );
+
+	drawSetColor( Scheme::SC_SECONDARY2 );
+	drawFilledRect( 0, 0, wide, tall );
+
+	int segmentGap = 2;
+	int segmentWide = wide / _segmentCount - segmentGap;
+	int litSeg = (int)_progress;
+	int x = 0;
+
+	for( int i = 0; i < litSeg; i++ )
+	{
+		drawSetColor( 0, 0, 100, 0 );
+		drawFilledRect( x, 0, x + segmentWide, tall );
+		x += segmentWide + segmentGap;
+	}
+
+	if( _segmentCount > _progress )
+	{
+		float frac = _progress - (int)_progress;
+
+		drawSetColor( 0, 0, 255 - frac * 155, 0 );
+		drawFilledRect( x, 0, x + segmentWide, tall );
+	}
 }
 
 void ProgressBar::setProgress( float progress )
 {
+	if( progress != _progress )
+	{
+		_progress = progress;
+		repaint();
+	}
 }
 
 int ProgressBar::getSegmentCount()
 {
-	return 0;
+	return _segmentCount;
 }
