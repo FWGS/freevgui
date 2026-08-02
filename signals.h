56a69a2c5ba0db6c4a843809ed8c9462267f905b
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-03 00:55:44 +0500

    signal: add some more anti-boilerplate classes

--- signals.h
+++ signals.h
@@ -17,27 +17,6 @@ public:
 	virtual void actionPerformed( Panel * ) = 0;
 };
 
-template <typename F>
-class LambdaActionSignal : public ActionSignal
-{
-	F func;
-public:
-	LambdaActionSignal( F f ) : func( f )
-	{
-	}
-
-	void actionPerformed( Panel *panel ) override
-	{
-		func( panel );
-	}
-};
-
-template <typename F>
-LambdaActionSignal<F> *makeActionHandler( F func )
-{
-	return new LambdaActionSignal<F>( func );
-}
-
 class CLASSEXPORT ChangeSignal {
 public:
 	virtual void valueChanged( Panel * ) = 0;
@@ -82,6 +61,150 @@ public:
 class CLASSEXPORT TickSignal {
 public:
 	virtual void ticked() = 0;
+};
+
+// ====== FreeVGUI extensions ======
+
+template <typename F>
+class LambdaActionSignal : public ActionSignal
+{
+	F func;
+public:
+	LambdaActionSignal( F f ) : func( f )
+	{
+	}
+
+	void actionPerformed( Panel *panel ) override
+	{
+		func( panel );
+	}
+};
+
+template <typename F>
+LambdaActionSignal<F> *makeActionHandler( F func )
+{
+	return new LambdaActionSignal<F>( func );
+}
+
+template <typename F>
+class LambdaChangeSignal : public ChangeSignal
+{
+	F func;
+public:
+	LambdaChangeSignal( F f ) : func( f )
+	{
+	}
+
+	void valueChanged( Panel *panel ) override
+	{
+		func( panel );
+	}
+};
+
+template <typename F>
+LambdaChangeSignal<F> *makeChangeHandler( F func )
+{
+	return new LambdaChangeSignal<F>( func );
+}
+
+template <typename F>
+class LambdaIntChangeSignal : public IntChangeSignal
+{
+	F func;
+public:
+	LambdaIntChangeSignal( F f ) : func( f )
+	{
+	}
+
+	void intChanged( int value, Panel *panel ) override
+	{
+		func( value, panel );
+	}
+};
+
+template <typename F>
+LambdaIntChangeSignal<F> *makeIntChangeHandler( F func )
+{
+	return new LambdaIntChangeSignal<F>( func );
+}
+
+template <typename F>
+class LambdaFocusChangeSignal : public FocusChangeSignal
+{
+	F func;
+public:
+	LambdaFocusChangeSignal( F f ) : func( f )
+	{
+	}
+
+	void focusChanged( bool state, Panel *panel ) override
+	{
+		func( state, panel );
+	}
+};
+
+template <typename F>
+LambdaFocusChangeSignal<F> *makeFocusChangeHandler( F func )
+{
+	return new LambdaFocusChangeSignal<F>( func );
+}
+
+template <typename F>
+class LambdaRepaintSignal : public RepaintSignal
+{
+	F func;
+public:
+	LambdaRepaintSignal( F f ) : func( f )
+	{
+	}
+
+	void panelRepainted( Panel *panel ) override
+	{
+		func( panel );
+	}
+};
+
+template <typename F>
+LambdaRepaintSignal<F> *makeRepaintHandler( F func )
+{
+	return new LambdaRepaintSignal<F>( func );
+}
+
+template <typename F>
+class LambdaTickSignal : public TickSignal
+{
+	F func;
+public:
+	LambdaTickSignal( F f ) : func( f )
+	{
+	}
+
+	void ticked() override
+	{
+		func();
+	}
+};
+
+template <typename F>
+LambdaTickSignal<F> *makeTickHandler( F func )
+{
+	return new LambdaTickSignal<F>( func );
+}
+
+class InputSignalAdapter : public InputSignal
+{
+public:
+	void cursorMoved( int, int, Panel * ) override {}
+	void cursorEntered( Panel * ) override {}
+	void cursorExited( Panel * ) override {}
+	void mousePressed( MouseCode, Panel * ) override {}
+	void mouseDoublePressed( MouseCode, Panel * ) override {}
+	void mouseReleased( MouseCode, Panel * ) override {}
+	void mouseWheeled( int, Panel * ) override {}
+	void keyPressed( KeyCode, Panel * ) override {}
+	void keyTyped( KeyCode, Panel * ) override {}
+	void keyReleased( KeyCode, Panel * ) override {}
+	void keyFocusTicked( Panel * ) override {}
 };
 }
 
