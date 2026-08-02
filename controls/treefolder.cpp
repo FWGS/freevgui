fdb573c55cf9154e7b28bc5844211c435e6f7f09
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-03 00:55:58 +0500

    controls: treefolder: implement TreeFolder

--- controls/treefolder.cpp
+++ controls/treefolder.cpp
@@ -8,82 +8,78 @@
 
 using namespace vgui;
 
-TreeFolder::TreeFolder( const char *name ) : Panel()
+namespace // tree-folder label toggle handler and vertical layout
 {
-}
-
-TreeFolder::TreeFolder( const char *name, int x, int y ) : Panel( x, y, 0, 0 )
+class TreeFolderLabelHandler : public InputSignalAdapter
 {
-}
-
-void TreeFolder::init( const char *name )
-{
-}
-
-void TreeFolder::paintBackground()
-{
-}
-
-void TreeFolder::setOpenedTraverse( bool open )
-{
-}
-
-void TreeFolder::setOpened( bool open )
-{
-}
-
-bool TreeFolder::isOpened()
-{
-	return false;
-}
-
-#if 0
-
-class FooTreeFolderDefaultHandler : public InputSignal
-{
-protected:
-	TreeFolder *_treeFolder;
+	TreeFolder *tf;
 public:
-	FooTreeFolderDefaultHandler( TreeFolder *tf ) :
-		_treeFolder( tf )
-	{
-	}
+	TreeFolderLabelHandler( TreeFolder *tf ) : tf( tf ) {}
 
-	void cursorMoved( int x, int y, Panel *p ) override;
-	void cursorEntered( Panel *p ) override;
-	void cursorExited( Panel *p ) override;
-	void mousePressed( MouseCode code, Panel *p ) override;
-	void mouseDoublePressed( MouseCode code, Panel *p ) override;
-	void mouseReleased( MouseCode code, Panel *p ) override;
-	void mouseWheeled( int y, Panel *p ) override;
-	void keyPressed( KeyCode code, Panel *p ) override;
-	void keyTyped( KeyCode code, Panel *p ) override;
-	void keyReleased( KeyCode code, Panel *p ) override;
-	void keyFocusTicked( Panel *p ) override;
+	virtual void mousePressed( MouseCode, Panel * ) override
+	{
+		tf->setOpened( !tf->isOpened( ));
+	}
 };
 
-class FooTabFolderVerticalLayout : public Layout
+class TreeFolderVerticalLayout : public Layout
 {
-protected:
-	int _hgap;
-	int _vgap;
+	int hgap, vgap;
+
 public:
-	FooTabFolderVerticalLayout( int hgap, int vgap ) :
-		_hgap( hgap ), _vgap( vgap )
+	TreeFolderVerticalLayout( int hgap, int vgap ) : hgap( hgap ), vgap( vgap ) {}
+
+	virtual void performLayout( Panel *panel ) override
 	{
+		int count = panel->getChildCount();
+		if( count == 0 )
+			return;
 
+		int y = 0, maxx = 0, labelWide = 0, labelTall = 0;
+
+		for( int i = 0; i < count; i++ )
+		{
+			Panel *child = panel->getChild( i );
+			int x = i == 0 ? 0 : hgap;
+
+			// nested folders re-lay-out before we measure them
+			TreeFolder *childFolder = dynamic_cast<TreeFolder *>( child );
+			if( childFolder )
+				childFolder->invalidateLayout( true );
+
+			int childWide, childTall;
+			child->getSize( childWide, childTall );
+			child->setPos( x, y );
+
+			y += childTall + vgap;
+			if( x + childWide > maxx )
+				maxx = x + childWide;
+
+			if( i == 0 )
+			{
+				labelWide = childWide;
+				labelTall = childTall;
+			}
+		}
+
+		TreeFolder *folder = dynamic_cast<TreeFolder *>( panel );
+		if( folder )
+		{
+			if( folder->isOpened( ))
+				panel->setSize( maxx + 2, y );
+			else
+				panel->setSize( labelWide, labelTall );
+		}
 	}
-
-	void performLayout( Panel *p ) override;
 };
-
-TreeFolder::TreeFolder(const char *name) :
-	TreeFolder( name, 0, 0 )
-{
 }
 
-TreeFolder::TreeFolder(const char *name, int x, int y) :
-	Panel( x, y, 500, 500 )
+TreeFolder::TreeFolder( const char *name ) : Panel( 0, 0, 500, 500 )
+{
+	init( name );
+}
+
+TreeFolder::TreeFolder( const char *name, int x, int y ) : Panel( x, y, 500, 500 )
 {
 	init( name );
 }
@@ -92,42 +88,31 @@ void TreeFolder::init( const char *name )
 {
 	_opened = false;
 
-	Label *l = new Label( name, 0, 0 );
-	l->addInputSignal( new FooTreeFolderDefaultHandler( this ));
-	l->setParent( this );
+	Label *label = new Label( name, 0, 0 );
+	label->addInputSignal( new TreeFolderLabelHandler( this ));
+	label->setParent( this );
 
-	setLayout( new FooTabFolderVerticalLayout( 0x1e, 3 ));
+	setLayout( new TreeFolderVerticalLayout( 30, 3 ));
 }
 
-void TreeFolder::setOpenedTraverse(bool open)
-{
-	setOpened( true );
-
-	for( int i = 0; i < getChildCount(); i++ )
-	{
-		TreeFolder *tf = dynamic_cast<TreeFolder *>( getChild( i ));
-		if( tf )
-			tf->setOpenedTraverse( open );
-	}
-}
-
-void TreeFolder::setOpened(bool open)
+void TreeFolder::setOpened( bool open )
 {
 	if( _opened == open )
 		return;
 
 	_opened = open;
 
+	// invalidate layout up the folder chain, tracking the topmost folder
 	TreeFolder *top = nullptr;
 
 	for( Panel *p = this; p; p = p->getParent( ))
 	{
-		TreeFolder *tf = dynamic_cast<TreeFolder*>( p );
+		TreeFolder *folder = dynamic_cast<TreeFolder *>( p );
 
-		if( tf )
+		if( folder )
 		{
-			tf->invalidateLayout( true );
-			top = tf;
+			folder->invalidateLayout( true );
+			top = folder;
 		}
 	}
 
@@ -135,8 +120,73 @@ void TreeFolder::setOpened(bool open)
 		top->repaintParent();
 }
 
+void TreeFolder::setOpenedTraverse( bool open )
+{
+	// quirk: the local node is always opened
+	setOpened( true );
+
+	for( int i = 0; i < getChildCount(); i++ )
+	{
+		TreeFolder *folder = dynamic_cast<TreeFolder *>( getChild( i ));
+
+		if( folder )
+			folder->setOpenedTraverse( open );
+	}
+}
+
 bool TreeFolder::isOpened()
 {
 	return _opened;
 }
-#endif
+
+void TreeFolder::paintBackground()
+{
+	int count = getChildCount();
+	if( count <= 1 )
+		return;
+
+	int x0 = 15, x1 = 30, y0 = 10;
+	int y1 = y0;
+
+	drawSetColor( Scheme::SC_BLACK );
+
+	// horizontal stubs from the spine out to each child
+	for( int i = 1; i < count; i++ )
+	{
+		Panel *child = getChild( i );
+		int cx, cy, cw, ch;
+		child->getBounds( cx, cy, cw, ch );
+
+		TreeFolder *folder = dynamic_cast<TreeFolder *>( child );
+		int y = folder ? cy + 10 : cy + ch / 2;
+
+		drawFilledRect( x0, y, x1, y + 1 );
+		y1 = y;
+	}
+
+	// vertical spine down to the last stub
+	drawFilledRect( x0, y0, x0 + 1, y1 );
+
+	// expand/collapse boxes over the spine for each sub-folder
+	for( int i = 1; i < count; i++ )
+	{
+		Panel *child = getChild( i );
+
+		TreeFolder *folder = dynamic_cast<TreeFolder *>( child );
+		if( !folder )
+			continue;
+
+		int cx, cy, cw, ch;
+		child->getBounds( cx, cy, cw, ch );
+		int y = cy + 10;
+
+		drawSetColor( Scheme::SC_WHITE );
+		drawFilledRect( x0 - 5, y - 5, x0 + 6, y + 6 );
+		drawSetColor( Scheme::SC_BLACK );
+		drawOutlinedRect( x0 - 5, y - 5, x0 + 6, y + 6 );
+		drawFilledRect( x0 - 3, y, x0 + 4, y + 1 );
+
+		if( !folder->isOpened( ))
+			drawFilledRect( x0, y - 3, x0 + 1, y + 4 );
+	}
+}
