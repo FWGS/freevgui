2d34908daf1b1c191801868a25892967055d8195
Alibek Omarov <a1ba.omarov@gmail.com>
2026-08-02 23:55:11 +0500

    platform: posix: fix getTimeMillis

--- platform/posix/app.cpp
+++ platform/posix/app.cpp
@@ -23,7 +23,7 @@ long int App::getTimeMillis()
 
 	clock_gettime( CLOCK_MONOTONIC, &ts );
 
-	return ts.tv_sec * 1000000 + ts.tv_nsec / 1000.0f;
+	return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 }
 
 void App::platTick()
